8db32e95ddbcffaf234b26b26dd16afde29ed3c5
xiefeng <1294699027@qq.com>
2023-11-02 21:46:29 +0800

    fix: fix build error

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -10,6 +10,7 @@ export default function GlobalError({ error, reset }: GlobalErrorProps) {
     <html>
       <body>
         <h2>Something went wrong!</h2>
+        <pre>{error.message}</pre>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
